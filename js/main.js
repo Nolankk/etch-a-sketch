aa9ed495653ff8d4e55727459849182e77fe127b
nolan <nolanpolak@gmail.com>
2022-07-13 14:39:23 -0500

    -Add gradient background -Centered items -Add header -Add footer -Make grid colorful

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
-const container = document.getElementById('container');
+const gridContainer = document.getElementById('gridContainer');
 const changeGrid = document.getElementById('changeGrid');
 let grid = null;
+function randomColor() {
+    return Math.floor(Math.random()*16777215).toString(16);
+}
 
 function createGrid(gridSize = 16) {
     for (let i = 0; i<gridSize**2; i++)
@@ -9,12 +12,12 @@ function createGrid(gridSize = 16) {
         newDiv.id = 'grid';
         newDiv.style.width = 600/gridSize + 'px';
         newDiv.style.height = 600/gridSize + 'px';
-        container.appendChild(newDiv);
+        gridContainer.appendChild(newDiv);
     }
     const grid = document.querySelectorAll('#grid');
     grid.forEach(item => {
         item.addEventListener('mouseover', event => {
-            event.target.style.backgroundColor = 'black';
+            event.target.style.backgroundColor = '#'+ randomColor();
         });
     });
     return grid;
@@ -35,7 +38,7 @@ changeGrid.onclick = function() {
     if (gridSize > 100) {
         gridSize = prompt('How large should the grid be (?x?)?');
     }
-    
+
     removeGrid(grid);
     grid = createGrid(gridSize);
 }
